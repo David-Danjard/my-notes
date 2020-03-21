338e195c875e7c9e760eb97893118f523eb1bf71
david.danjard@gmail.com <A45y22s@>
2020-03-21 22:34:57 +0100

    Initial commit - project creation

---
+++ users/src/test/java/com/my/notes/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.notes.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
